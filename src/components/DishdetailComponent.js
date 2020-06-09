31caad636cbb79b2b3f31e044a22a0053ad4a364
Naman Sukhwani <namansukhwani@gmail.com>
2020-06-09 17:14:00 +0530

    Assignment 3 Redux form and Modal

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,105 @@
 import React from 'react';
 import '../App.css';
-import { Card, CardImg, CardBody, CardTitle, CardText ,Breadcrumb ,BreadcrumbItem} from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText ,Breadcrumb ,BreadcrumbItem ,Button, Modal, ModalHeader, ModalBody, Label,Row} from 'reactstrap';
+import { LocalForm, Control,Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
+
+const maxLength=(len) => (val) => !(val) || (val.length <= len);
+const minLength=(len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            isModalOpen:false
+        };
+        this.toggleModal=this.toggleModal.bind(this);
+        this.handelSubmit=this.handelSubmit.bind(this);
+    }
+
+    toggleModal()
+    {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handelSubmit(values)
+    {
+        alert("current state is " + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil"></span>{' '}Submit Comment</Button>
+            
+
+                {/*--Modal For Comment--*/}
+
+                <Modal id="commentModal" isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handelSubmit} >
+                            <Row className="form-row mt-2">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating"
+                                id="rating"
+                                name="rating"
+                                className="form-control"
+                                >
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="form-row mt-2">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author"
+                                id="author"
+                                name="author"
+                                className="form-control"
+                                placeholder="Your Name"
+                                validators={{
+                                    minLength:minLength(3),
+                                    maxLength:maxLength(15)
+                                }}
+                                />
+                                <Errors 
+                                className="text-danger"
+                                model=".author"
+                                show="touched"
+                                messages={{
+                                    minLength:'Must be greater than 2 characters',
+                                    maxLength:'Must be 15 characters or less'
+                                }}/>
+                            </Row>
+                            <Row className="form-row mt-2">
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".comment"
+                                rows="6"
+                                id="comment"
+                                name="comment"
+                                className="form-control"
+                                />
+                            </Row>
+                            <Row className="form-row mt-2">
+                                <Button type="submit" color="primary" >Submit</Button>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+
+            </React.Fragment>
+        );
+    }
+}
+
 
     function RenderDish({dish})
     {
@@ -21,8 +119,7 @@ import {Link} from 'react-router-dom';
     function RenderComments({comments})
     {
         if(comments!=null)
-        {
-            
+        {   
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>comments</h4>
@@ -30,12 +127,13 @@ import {Link} from 'react-router-dom';
                         {comments.map(comment => {
                             return(
                             <li  key={comment.id}>
-                                <p>{comment.comment}</p><br/>
-                                <p>--{comment.author} ,{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p><br/>
+                                <p>{comment.comment}</p>
+                                <p>--{comment.author} ,{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
                             );
                         })}
                     </ul>
+                    <CommentForm/>
                 </div>
             );
         }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,7 +31,7 @@ class Main extends React.Component {
     const DishWithId=({match}) => {
       return(
         <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-         comments={this.props.comments.filter((comment) => comment.id === parseInt(match.params.dishId,10))} />
+         comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
     
